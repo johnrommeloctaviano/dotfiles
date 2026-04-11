0330ede9390a591af5847932eef5969a58c12c1e
johnrommeloctaviano <johnrommel.octaviano@gmail.com>
2026-04-12 05:15:52 +0800

    fix: Update Neovim theme

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,47 +1,7 @@
 return {
-  {
-    "tiesen243/vercel.nvim",
-    lazy = false,
-    opts = {
-      theme = "dark",
-      transparent = false,
-      italics = {
-        comments = false,
-        keywords = false,
-        strings = false,
-        functions = false,
-        variables = false,
-        bufferline = false,
-      },
-    },
-    config = function(_, opts)
-      require("vercel").setup(opts)
-      vim.cmd.colorscheme("vercel")
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "*",
-        callback = function()
-          local black = "#000000"
-          local groups = {
-            "Normal",
-            "NormalNC",
-            "NormalFloat",
-            "SignColumn",
-            "LineNr",
-          }
-          for _, group in ipairs(groups) do
-            vim.api.nvim_set_hl(0, group, { bg = black })
-          end
-        end,
-      })
-      vim.defer_fn(function()
-        vim.cmd("colorscheme vercel")
-      end, 50)
-    end,
-  },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "vercel",
-    },
-  },
+  'projekt0n/github-nvim-theme',
+  name = 'github-theme',
+  config = function()
+    vim.cmd('colorscheme github_dark_default')
+  end,
 }
